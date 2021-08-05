c1b426e727765dc2bd0e2500d77160bf50d99fd7
MassiGy <ghernaoutmassi@gmail.com>
2021-08-05 16:15:02 +0100

    sendEmail func & endpoint  added

--- app.js
+++ app.js
@@ -5,28 +5,40 @@ const ejs_mate = require('ejs-mate');
 const ejs = require('ejs');
 const path = require('path');
 const axios = require('axios');
-const option = { headers: { accept: 'application/json' } };
+const { stringify } = require('postcss');
+const { json } = require('express');
+const mailjet = require('node-mailjet').connect(process.env.apiConnectFirstArg, process.env.apiConnectSecondArg)
 const port = process.env.PORT || 3000;
+const option = { headers: { accept: 'application/json' } };
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
 
 
-
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.engine('ejs', ejs_mate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use('/css', express.static('css'));
-
+app.use('/partials', express.static('partials'));
 
 app.get('/', (req, res) => {
     let response = {};
-    res.render('main', { response });
+    res.render('home', { response });
+})
+
+app.get('/home', (req, res) => {
+    let response = {};
+    res.render('home', { response });
 })
 
 
 app.post('/test', (req, res) => {
     axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.apiKey}`, option)
         .then((response) => {
-            res.render('main', { response })
+            res.render('home', { response })
         })
         .catch(err => {
             console.log(err.message);
@@ -37,6 +49,47 @@ app.post('/test', (req, res) => {
 app.post('/reset', (req, res) => {
     res.redirect('/');
 })
+
+app.get('/sendEmail', (req, res) => {
+    res.render('sendEmail');
+})
+
+
+app.post('/sendEmail', (req, res) => {
+    console.log(req.body)
+    axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.apiKey}`, option)
+        .then((response) => {
+            const request = mailjet
+                .post("send", { 'version': 'v3.1' })
+                .request({
+                    "Messages": [{
+                        "From": {
+                            "Email": "ghernaoutmassi@gmail.com",
+                            "Name": "Massi gy"
+                        },
+                        "To": [{
+                            "Email": req.body.Email_address,
+                            "Name": req.body.Username
+                        }],
+                        "Subject": "Vpn Test Resaults",
+                        "TextPart": JSON.stringify(response.data),
+                    }]
+                })
+            request
+                .then((result) => {
+                    res.send(result.body.Messages[0].Status)
+                })
+                .catch((err) => {
+                    res.send(err.message)
+                })
+        })
+        .catch(err => {
+            res.send(err);
+        })
+
+
+})
+
 
 
 app.listen(port, (req, res) => {
